caf17408b6dfc3a53786c5ce5911047d92bf32b9
unknown <MoodleUser@CPPQ238.cppq.com.pe>
2020-11-09 20:05:47 -0500

    'frontend1'

---
+++ local/qroma/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once(dirname(__FILE__) . '/../../config.php');
+
+global $PAGE, $OUTPUT;
+
+$title = 'Home';
+// Set up the page.
+$url = new moodle_url("/local/qroma/index.php");
+$PAGE->set_url($url);
+
+$PAGE->requires->css(new moodle_url('css/_base.css'));
+$PAGE->requires->css(new moodle_url('css/general.css'));
+
+$PAGE->requires->jquery();
+//$PAGE->requires->js(new moodle_url('js/main.js'));
+
+echo $OUTPUT->header();
+include('home.php');
