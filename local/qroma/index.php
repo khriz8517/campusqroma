7670a25835d4937f1b1db1e58ad97cf9b3260d18
unknown <MoodleUser@CPPQ238.cppq.com.pe>
2020-11-09 20:40:04 -0500

    'frontend1'

--- local/qroma/index.php
+++ local/qroma/index.php
@@ -12,7 +12,8 @@ $PAGE->requires->css(new moodle_url('css/_base.css'));
 $PAGE->requires->css(new moodle_url('css/general.css'));
 
 $PAGE->requires->jquery();
-//$PAGE->requires->js(new moodle_url('js/main.js'));
+$PAGE->requires->js(new moodle_url('js/main.js'));
 
 echo $OUTPUT->header();
 include('home.php');
+echo $OUTPUT->footer();
---
+++ local/qroma/js/main.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello Vue!'
+    }
+});
+let options = {
+    startAngle: -1.55,
+    size: 150,
+    value: 0.85,
+    fill: {gradient: ['#a445b2', '#fa4299']}
+}
+$(".circle .bar").circleProgress(options).on('circle-animation-progress',
+    function(event, progress, stepValue){
+        $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+    });
+$(".js .bar").circleProgress({
+    value: 0.70
+});
+$(".react .bar").circleProgress({
+    value: 0.60
+});
